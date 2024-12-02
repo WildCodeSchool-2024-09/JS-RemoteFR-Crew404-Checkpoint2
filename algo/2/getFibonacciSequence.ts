549784738849bccec2c50bdff286b1f902f8d3c2
CAMILLEG <cgyrd@outlook.fr>
2024-12-02 10:28:32 +0100

    algo 2 check passed

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -1,3 +1,4 @@
+import "./getFibonacciSequence.spec";
 /*
 Créé une fonction getFibonacciSequence qui prend un nombre n en paramètre et retourne un tableau contenant les n premiers nombres de la suite de Fibonacci.
 
@@ -10,9 +11,25 @@ Détails
 Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
-function getFibonacciSequence(size: number): number[] {
-  // Your code here !
-  return [0, 1];
+function getFibonacciSequence(n: number): number[] {
+  // pour le retour à 0 si le tableau est vide
+  if (n <= 0) {
+    return [];
+  }
+
+  // lancement des deux nombres O et 1
+  const startsequence = [0, 1];
+  // retour à zéro si ce n'es pas le cas
+  if (n === 1) {
+    return [0];
+  }
+
+  //nombre = somme des précedents
+  for (let i = 2; i < n; i++) {
+    startsequence[i] = startsequence[i - 1] + startsequence[i - 2];
+  }
+
+  return startsequence.slice(0, n);
 }
 
 export default getFibonacciSequence;
