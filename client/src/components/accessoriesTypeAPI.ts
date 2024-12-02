20db9a2f221a43d2577aa39f7cf23d0d446b078f
Ryan DECIAN <ryan.decian.propgmail.com>
2024-12-02 14:07:38 +0100

    Checkpoint2

---
+++ client/src/components/accessoriesTypeAPI.ts
@@ -0,0 +1,5 @@
+export interface accessoriesTypeAPI {
+  id: number;
+  name: string;
+  slug: string;
+}
--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -1,71 +1,102 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Cupcake from "../components/Cupcake";
+import type { accessoriesTypeAPI } from "../components/accessoriesTypeAPI";
+import type { cupcakeTypeAPI } from "../components/cupcakeTypeAPI";
 
-/* ************************************************************************* */
-const sampleCupcakes = [
-  {
-    id: 10,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "blue",
-    color2: "white",
-    color3: "red",
-    name: "France",
-  },
-  {
-    id: 11,
-    accessory_id: "4",
-    accessory: "wcs",
-    color1: "yellow",
-    color2: "red",
-    color3: "black",
-    name: "Germany",
-  },
-  {
-    id: 27,
-    accessory_id: "5",
-    accessory: "christmas-candy",
-    color1: "yellow",
-    color2: "blue",
-    color3: "blue",
-    name: "Sweden",
-  },
-];
-
-type CupcakeArray = typeof sampleCupcakes;
-
-/* you can use sampleCupcakes if you're stucked on step 1 */
-/* if you're fine with step 1, just ignore this ;) */
-/* ************************************************************************* */
+// Définir le type pour les cupcakes
+type CupcakeType = {
+  id: number;
+  accessory_id: string;
+  accessory: string;
+  color1: string;
+  color2: string;
+  color3: string;
+  name: string;
+};
 
 function CupcakeList() {
-  // Step 1: get all cupcakes
-  console.info(useLoaderData() as CupcakeArray);
+  // États pour stocker les données et le statut de chargement
+  const [cupcakes, setCupcakes] = useState<cupcakeTypeAPI[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [accessories, setAccessories] = useState<accessoriesTypeAPI[]>([]);
+  const [selectAccessory, setSelectAccessory] = useState<string>("");
 
-  // Step 3: get all accessories
+  useEffect(() => {
+    const dataCupcakeAPI = async (): Promise<void> => {
+      const response = await fetch("http://localhost:3310/api/cupcakes");
+      const data: CupcakeType[] = await response.json();
+      setCupcakes(data);
+      setLoading(false);
+    };
 
-  // Step 5: create filter state
+    dataCupcakeAPI();
+  }, []);
+
+  useEffect(() => {
+    const dataAccessoriesAPI = async (): Promise<void> => {
+      const response = await fetch("http://localhost:3310/api/accessories");
+      const data: accessoriesTypeAPI[] = await response.json();
+      setAccessories(data);
+      setLoading(false);
+    };
+
+    dataAccessoriesAPI();
+  }, []);
+
+  if (loading) {
+    return <p>Loading cupcakes...</p>;
+  }
+
+  // Filtrer les cupcakes par accessoire sélectionné
+  const filterCupcakes = selectAccessory
+    ? cupcakes.filter((cupcake) => cupcake.accessory === selectAccessory)
+    : cupcakes;
+
+  // Gestion du changement de sélection des accessoire
+  const AccessoryChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectAccessory(event.target.value);
+  };
 
   return (
     <>
       <h1>My cupcakes</h1>
       <form className="center">
         <label htmlFor="cupcake-select">
-          {/* Step 5: use a controlled component for select */}
           Filter by{" "}
-          <select id="cupcake-select">
+          <select
+            id="cupcake-select"
+            value={selectAccessory}
+            onChange={AccessoryChange}
+          >
             <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
+            {accessories.map((accessory) => (
+              <option key={accessory.id} value={accessory.slug}>
+                {accessory.name}
+              </option>
+            ))}
+            {/*[...new Set(cupcakes.map((cupcake) => cupcake.accessory))].map(
+              (accessory, index) => (
+                <option key={index} value={accessory}>
+                  {accessory}
+                </option>
+              )
+            )*/}
           </select>
         </label>
       </form>
       <ul className="cupcake-list" id="cupcake-list">
-        {/* Step 2: repeat this block for each cupcake */}
-        {/* Step 5: filter cupcakes before repeating */}
-        <li className="cupcake-item">
-          <Cupcake data={sampleCupcakes[0]} />
-        </li>
-        {/* end of block */}
+        {/*Test Affichage des cupcakes récupérés */}
+        {filterCupcakes.map((cupcake) => (
+          <li className="cupcake-item" key={cupcake.id}>
+            <Cupcake data={cupcake} />
+          </li>
+        ))}
+
+        {/*cupcakes.map((cupcake) => (
+          <li className="cupcake-item" key={cupcake.id}>
+            <Cupcake data={cupcake} />
+          </li>
+        ))*/}
       </ul>
     </>
   );
